02fd5592552bdb7a3f922c5b6a95c8606910002c
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-06-25 09:24:33 +0530

    Add: Start Buying a book

--- src/main/java/com/sdb/tdd/CalculateDiscount.java
+++ src/main/java/com/sdb/tdd/CalculateDiscount.java
@@ -1,5 +1,12 @@
 package com.sdb.tdd;
 
 public class CalculateDiscount {
+	
+	public double getTotalPrice(double dis) {
+		double amtAfterDis = 0.00;
+		amtAfterDis = 50 - (50 * (dis / 100));
+		return amtAfterDis;
+	}
+
 
 }
